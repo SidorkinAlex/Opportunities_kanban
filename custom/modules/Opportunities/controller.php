1e9a3a1b6188ab0878218565aedb95b10a1e63b7
SidorkinAlex <palach.orl@mail.ru>
2020-08-26 17:24:53 +0300

    add custom action quickDetail from Opportunities. This action must display the deal Opportunity for the popup

---
+++ custom/modules/Opportunities/controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: seedteam
+ * Date: 26.08.20
+ * Time: 16:01
+ */
+class CustomOpportunitiesController extends SugarController
+{
+     public function action_quickDetail(){
+         $this->view = 'detail';
+     }
+
+}
