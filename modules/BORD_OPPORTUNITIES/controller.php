287a12595e16f09bb5cd9af5ab79b068ef6fbc5a
SidorkinAlex <palach.orl@mail.ru>
2020-08-27 17:32:39 +0300

    add action in module BORD_OPPORTUNITIES from returm Opportunities list

---
+++ modules/BORD_OPPORTUNITIES/controller.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: seedteam
+ * Date: 27.08.20
+ * Time: 16:11
+ */
+class CustomBORD_OPPORTUNITIESController extends SugarController
+{
+    public function action_getData()
+    {
+
+        $order_by='date_entered DESC';
+        if($_REQUEST['where']) {
+            $in = "'" . implode("','",$_REQUEST['where']) . "'";
+            $where = '(opportunities.sales_stage in (' . $in . '))';
+        }
+        $filter=array (
+            'sales_stage' => true,
+        );
+        $params=array (
+            'massupdate' => true,
+            'orderBy' => 'DATE_ENTERED',
+            'overrideOrder' => true,
+            'sortOrder' => 'DESC',
+        );
+        $show_deleted=0;
+        $join_type='';
+        $return_array=true;
+        $singleSelect=true;
+        $ifListForExport=false;
+        $parentbean= new Opportunity();
+        $been= new Opportunity();
+        $create_new_list_query=$been->create_new_list_query(
+            $order_by,
+            $where,
+            $filter,
+            $params,
+            $show_deleted,
+            $join_type,
+            $return_array,
+            $parentbean,
+            $singleSelect,
+            $ifListForExport
+        );
+        print_array($create_new_list_query);
+        $create_new_list_query['select']=' SELECT opportunities.`id` as `opportunities_id`, CONCAT(opportunities.`name`) as `opportunities_name`, opportunities.`sales_stage` as `opportunities_sales_stage`';
+        print_array($create_new_list_query['select'] . $create_new_list_query['from'] . $create_new_list_query['where'] . $create_new_list_query['order_by']);
+    }
+}
