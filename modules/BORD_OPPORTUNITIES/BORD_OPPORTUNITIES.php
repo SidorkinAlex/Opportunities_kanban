968a630a8f5b5a0c462e641282c8b4fce8b9c01b
SidorkinAlex <palach.orl@mail.ru>
2020-09-02 22:46:36 +0300

    refactoring

--- modules/BORD_OPPORTUNITIES/BORD_OPPORTUNITIES.php
+++ modules/BORD_OPPORTUNITIES/BORD_OPPORTUNITIES.php
@@ -157,4 +157,73 @@ class BORD_OPPORTUNITIES extends Basic
         $result = $db->getOne($sql,1);
         return $result;
     }
+
+    /**
+     * get data Oportiunities from kanbanbord
+     * @param array $whereArr
+     * @return array
+     */
+    public function getDataOpp($whereArr){
+
+        global $db;
+
+        $bordConfig=BORD_OPPORTUNITIES::getBordConfig();
+        $order_by='date_entered DESC';
+        if($whereArr) {
+            $in = "'" . implode("','",$whereArr) . "'";
+            $where = '(opportunities.sales_stage in (' . $in . '))';
+        } else {
+            $stagesDefault=[];
+            foreach ($bordConfig['stages'] as $v){
+                if($v['show'])
+                    $stagesDefault[]=$v['name'];
+            }
+            $in = "'" . implode("','",$stagesDefault) . "'";
+            $where = '(opportunities.sales_stage in (' . $in . '))';
+        }
+        $filter=array (
+            'sales_stage' => true,
+        );
+        $params=array (
+            'massupdate' => true,
+            'orderBy' => 'DATE_ENTERED',
+            'overrideOrder' => true,
+            'sortOrder' => 'DESC',
+        );
+        $show_deleted=0;
+        $join_type='';
+        $return_array=true;
+        $singleSelect=true;
+        $ifListForExport=false;
+        $parentbean= new Opportunity();
+        $been= new Opportunity();
+        $create_new_list_query=$been->create_new_list_query(
+            $order_by,
+            $where,
+            $filter,
+            $params,
+            $show_deleted,
+            $join_type,
+            $return_array,
+            $parentbean,
+            $singleSelect,
+            $ifListForExport
+        );
+        $mainFields= '`opportunities`.' . implode(', " " ,`opportunities`.',$bordConfig['mainFields']);
+        //print_array($create_new_list_query);
+        $create_new_list_query['select']=' SELECT opportunities.`id` as `opportunities_id`, CONCAT(' . $mainFields . ') as `opportunities_name`, opportunities.`sales_stage` as `opportunities_sales_stage`';
+        //print_array($create_new_list_query['select'] . $create_new_list_query['from'] . $create_new_list_query['where'] . $create_new_list_query['order_by']);
+        $sql=$create_new_list_query['select'] . $create_new_list_query['from'] . $create_new_list_query['where'] . $create_new_list_query['order_by'];
+
+        $result=$db->query($sql,1);
+        $data=[];
+        while ($row = $db->fetchByAssoc($result)) {
+            $data[$row['opportunities_sales_stage']][]=[
+                'id' => $row['opportunities_id'],
+                'opportunities_name' => $row['opportunities_name'],
+                'opportunities_sales_stage' => $row['opportunities_sales_stage'],
+            ];
+        }
+        return $data;
+    }
 }
